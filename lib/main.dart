ea9306c415b548b93f64eafb8537884ad439775f
ittahadzaman <ittahad.zaman@selise.ch>
2021-07-02 13:28:10 +0600

    I am poor challenge

--- lib/main.dart
+++ lib/main.dart
@@ -3,52 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+              child: Text(
+            'I  Am  Poor',
+            style: TextStyle(fontFamily: "Qahiri", fontSize: 40),
+          )),
+          backgroundColor: Colors.deepOrangeAccent,
         ),
         backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image(
-                  width: 200,
-                  height: 300,
-                  image: AssetImage('images/Supermanflying.png'),
+        body: Stack(children: <Widget>[
+          Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+            Image(
+              image: AssetImage('images/brokeman.jpg'),
+            ),
+            Image(
+              image: AssetImage('images/coding.jpg'),
+            ),
+          ]),
+          Center(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 90, 0, 0),
+              child: Text(
+                'but, I love to code',
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 40,
+                  color: Colors.deepOrangeAccent,
                 ),
-                Container(
-                  child: Transform.scale(
-                    scale: 0.5,
-                    child: Stack(children: <Widget>[
-                      Image(
-                        width: 250,
-                        height: 250,
-                        color: Colors.red,
-                        image: AssetImage('images/diamond.png'),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(25, 25, 0, 0),
-                        child: Image(
-                          width: 200,
-                          height: 200,
-                          color: Colors.amberAccent,
-                          image: AssetImage('images/diamond.png'),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(82, 40, 0, 0),
-                        child: Text(
-                          "S",
-                          style: TextStyle(fontSize: 150, color: Colors.red),
-                        ),
-                      ),
-                    ]),
-                  ),
-                ),
-              ]),
-        ),
+              ),
+            ),
+          ),
+        ]),
       ),
     ),
   );
