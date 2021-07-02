740871f0be3f71d350a0ac26ab0382c459e141f5
ittahadzaman <ittahad.zaman@selise.ch>
2021-07-02 22:30:26 +0600

    dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   return runApp(
@@ -15,9 +19,78 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rand = 1;
+  int rand2 = 1;
+  int current = 0;
+
+  var imageName = 'images/dice1.png';
+  var imageName2 = 'images/dice1.png';
+  void generateRandomNumber(int player) {
+    setState(() {
+      rand = Random().nextInt(5) + 1;
+      imageName = 'images/dice$rand.png';
+
+      rand2 = Random().nextInt(5) + 1;
+      imageName2 = 'images/dice$rand2.png';
+
+      if (rand == rand2) {
+        Fluttertoast.showToast(
+            msg: "Player $player won",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Player 1",
+                  style: TextStyle(color: Colors.white),
+                ),
+                TextButton(
+                  child: Image.asset(imageName),
+                  onPressed: () => generateRandomNumber(1),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Player 2',
+                  style: TextStyle(color: Colors.white),
+                ),
+                TextButton(
+                  child: Image.asset(imageName2),
+                  onPressed: () => generateRandomNumber(2),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
